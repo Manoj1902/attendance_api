ef2e0c830cc93b0fd2be960debc862d934229586
manoj1902 <raj.manoj34@gmail.com>
2024-06-22 17:25:33 +0530

    test commit

--- fetch.php
+++ fetch.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST");
 header("Access-Control-Allow-Headers: Content-Type");
 
-// Database connection
+// Database connection--
 $CN = mysqli_connect("localhost", "root", "", "attendance");
 
 if (!$CN) {

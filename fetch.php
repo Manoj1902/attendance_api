7f4509291f91acfea11c3b125a4ab3b421fda740
manoj1902 <raj.manoj34@gmail.com>
2024-07-01 17:55:38 +0530

    third

--- fetch.php
+++ fetch.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST");
 header("Access-Control-Allow-Headers: Content-Type");
 
-// Database connection--
+// Database connection
 $CN = mysqli_connect("localhost", "root", "", "attendance");
 
 if (!$CN) {
@@ -13,7 +13,7 @@ if (!$CN) {
 }
 
 // Server URL (modify this to your server URL)
-$server_url = 'http://192.168.137.1/uploads/';
+$server_url = 'http://192.168.137.1/api/';
 
 // SQL query to fetch all details from the 'employee' table
 $IQ = "SELECT * FROM employee";
@@ -23,14 +23,18 @@ $Response = array();
 
 // Fetch data from the database
 while ($Row = mysqli_fetch_assoc($R)) {
-    $Row['Image'] = $server_url . $Row['Image']; // Append the server URL to the image path
+    // Only prepend the server URL if it's not already present
+    if (strpos($Row['Image'], $server_url) === false) {
+        $Row['Image'] = $server_url . $Row['Image'];
+    }
     $Response[] = $Row;
 }
 
 // Encode the response as JSON
-echo json_encode($Response);
+echo json_encode($Response, JSON_PRETTY_PRINT);
 
 // Close the database connection
 mysqli_close($CN);
+die();
 
 ?>
